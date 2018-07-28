1aee4f7c8fa64be20131a4861d599b7a0a3ee783
王山姆 <sakmor@gmail.com>
2018-07-28 13:50:50 +0800

    特效新增

--- Assets/script/biology.cs
+++ Assets/script/biology.cs
@@ -5,199 +5,216 @@ using UnityEngine;
 
 public class biology : MonoBehaviour
 {
-	[SerializeField] internal main main;
-	private float HitStopTime;
-	[SerializeField] internal bool IsPunchNext;
-	[SerializeField] internal bool IsMoveable = true;
-	private Vector3 GoalPos;
-	[SerializeField] private float Speed;
-	[SerializeField] private float MoveStep;
-	public bool isRandom;
-	[SerializeField] internal float LastAttackTime;
+    [SerializeField] internal main main;
+    private float HitStopTime;
+    [SerializeField] internal bool IsPunchNext;
+    [SerializeField] internal bool IsMoveable = true;
+    private Vector3 GoalPos;
+    [SerializeField] private float Speed;
+    [SerializeField] private float MoveStep;
+    public bool isRandom;
+    [SerializeField] internal float LastAttackTime;
 
-	[SerializeField] private AnimationState AnimationStates;
-	private Animator Animator;
-	public float IdleCrossFadeTime = 1f;
-	private IEnumerator coroutine;
-	public enum AnimationState
-	{
-		Idle, Walking, Running, Punching_1, Punching_2, HurtUp
-	}
-	void Start()
-	{
-		GoalPos = transform.position;
-		Animator = GetComponent<Animator>();
-		InvokeRepeating("randomMove", 1f, UnityEngine.Random.Range(3f, 5f));
-	}
-	void Update()
-	{
+    [SerializeField] private AnimationState AnimationStates;
+    private Animator Animator;
+    public float IdleCrossFadeTime = 1f;
+    public enum AnimationState
+    {
+        Idle, Walking, Running, Punching_1, Punching_2, HurtUp
+    }
+    void Start()
+    {
+        GoalPos = transform.position;
+        Animator = GetComponent<Animator>();
+        InvokeRepeating("randomMove", 1f, UnityEngine.Random.Range(3f, 5f));
+    }
+    void Update()
+    {
 
-	}
+    }
 
-	internal void PlayAnimation(AnimationState animationStates)
-	{
-		Animator.ResetTrigger("Idle");
-		if (animationStates == AnimationState.Idle)
-		{
-			if (AnimationStates == AnimationState.Idle) return;
-			Animator.SetTrigger("Idle");
-			Animator.CrossFade("Idle", 0.1f);
-			Animator.speed = 1;
-			SetAnimationStates(animationStates);
-			IsMoveable = true;
-		}
-		if (animationStates == AnimationState.Walking)
-		{
-			if (AnimationStates == AnimationState.Walking) { Animator.speed = Speed * MoveStep; return; }
-			Animator.CrossFade("Walking", 1.0f);
-			SetAnimationStates(animationStates);
-		}
-		if (animationStates == AnimationState.Running)
-		{
-			if (AnimationStates == AnimationState.Running) { Animator.speed = Speed * MoveStep; return; }
-			Animator.CrossFade("Running", 0.80f);
-			SetAnimationStates(animationStates);
-		}
-		if (animationStates == AnimationState.Punching_1)
-		{
-			if (AnimationStates == AnimationState.Punching_1) { return; }
-			if (AnimationStates == AnimationState.Punching_2) { return; }
-			Animator.CrossFade("Punching_1", 0.1f);
-			Animator.speed = 1.5f;
-			SetAnimationStates(animationStates);
-			IsMoveable = false;
-		}
-		if (animationStates == AnimationState.Punching_2)
-		{
-			if (AnimationStates == AnimationState.Punching_2) { return; }
-			Animator.CrossFade("Punching_2", 0.1f);
-			Animator.speed = 1.5f;
-			SetAnimationStates(animationStates);
-			IsMoveable = false;
-		}
-		if (animationStates == AnimationState.HurtUp)
-		{
+    internal void PlayAnimation(AnimationState animationStates)
+    {
+        Animator.ResetTrigger("Idle");
+        if (animationStates == AnimationState.Idle)
+        {
+            if (AnimationStates == AnimationState.Idle) return;
+            Animator.SetTrigger("Idle");
+            Animator.CrossFade("Idle", 0.1f);
+            Animator.speed = 1;
+            SetAnimationStates(animationStates);
+            IsMoveable = true;
+        }
+        if (animationStates == AnimationState.Walking)
+        {
+            if (AnimationStates == AnimationState.Walking) { Animator.speed = Speed * MoveStep; return; }
+            Animator.CrossFade("Walking", 1.0f);
+            SetAnimationStates(animationStates);
+        }
+        if (animationStates == AnimationState.Running)
+        {
+            if (AnimationStates == AnimationState.Running) { Animator.speed = Speed * MoveStep; return; }
+            Animator.CrossFade("Running", 0.80f);
+            SetAnimationStates(animationStates);
+        }
+        if (animationStates == AnimationState.Punching_1)
+        {
+            if (AnimationStates == AnimationState.Punching_1) { return; }
+            if (AnimationStates == AnimationState.Punching_2) { return; }
+            Animator.CrossFade("Punching_1", 0.1f);
+            Animator.speed = 1.5f;
+            SetAnimationStates(animationStates);
+            IsMoveable = false;
+        }
+        if (animationStates == AnimationState.Punching_2)
+        {
+            if (AnimationStates == AnimationState.Punching_2) { return; }
+            Animator.CrossFade("Punching_2", 0.1f);
+            Animator.speed = 1.5f;
+            SetAnimationStates(animationStates);
+            IsMoveable = false;
+        }
+        if (animationStates == AnimationState.HurtUp)
+        {
 
-			Animator.CrossFade("HurtUp", 0.1f, 0, 0.1f);
-			Animator.speed = 1;
-			SetAnimationStates(animationStates);
-			IsMoveable = false;
-		}
-	}
+            Animator.CrossFade("HurtUp", 0.1f, 0, 0.1f);
+            Animator.speed = 1;
+            SetAnimationStates(animationStates);
+            IsMoveable = false;
+        }
+    }
 
-	internal void SetAnimationStates(AnimationState animationStates)
-	{
-		AnimationStates = animationStates;
-	}
+    internal void SetAnimationStates(AnimationState animationStates)
+    {
+        AnimationStates = animationStates;
+    }
 
-	void randomMove()
-	{
-		if (isRandom)
-		{
-			int n = UnityEngine.Random.Range(1, 4);
-			GoalPos = GameObject.Find("w" + n).transform.position; //fixme:不要在用Find了
-		}
-	}
+    void randomMove()
+    {
+        if (isRandom)
+        {
+            int n = UnityEngine.Random.Range(1, 4);
+            GoalPos = GameObject.Find("w" + n).transform.position; //fixme:不要在用Find了
+        }
+    }
 
-	internal void SetIsMoveableTrue()
-	{
-		IsMoveable = true;
-	}
+    internal void SetIsMoveableTrue()
+    {
+        IsMoveable = true;
+    }
 
-	internal void SetIsPunchNext()
-	{
-		IsPunchNext = true;
-		LastAttackTime = Time.time;
-	}
-	internal void MoveTo(Vector3 direct)
-	{
-		if (IsMoveable == false) return;
+    internal void SetIsPunchNext()
+    {
+        IsPunchNext = true;
+        LastAttackTime = Time.time;
+    }
+    internal void MoveTo(Vector3 direct)
+    {
+        if (IsMoveable == false) return;
 
-		GoalPos = transform.position + direct;
-		Speed = direct.magnitude;
+        GoalPos = transform.position + direct;
+        Speed = direct.magnitude;
 
-		transform.position = Vector3.MoveTowards(transform.position, GoalPos, Speed * Time.deltaTime);
-		faceTarget(GoalPos, Speed * 10.0f);
-		float threshold = 0.9f;
-		if (Speed <= threshold) PlayAnimation(AnimationState.Walking);
-		if (Speed > threshold) PlayAnimation(AnimationState.Running);
+        transform.position = Vector3.MoveTowards(transform.position, GoalPos, Speed * Time.deltaTime);
+        faceTarget(GoalPos, Speed * 10.0f);
+        float threshold = 0.9f;
+        if (Speed <= threshold) PlayAnimation(AnimationState.Walking);
+        if (Speed > threshold) PlayAnimation(AnimationState.Running);
 
-	}
+    }
 
-	internal void AttackButtonDown()
-	{
-		StopWalking();
-		if (Time.time - LastAttackTime < IdleCrossFadeTime)
-		{
-			PlayAnimation(AnimationState.Punching_2);
-		}
-		if (IsPunchNext == false && AnimationStates == AnimationState.Idle)
-		{
-			PlayAnimation(AnimationState.Punching_1);
-		}
+    internal void AttackButtonDown()
+    {
+        StopWalking();
+        if (Time.time - LastAttackTime < IdleCrossFadeTime)
+        {
+            PlayAnimation(AnimationState.Punching_2);
+        }
+        if (IsPunchNext == false && AnimationStates == AnimationState.Idle)
+        {
+            PlayAnimation(AnimationState.Punching_1);
+        }
 
-	}
+    }
 
-	internal void StopWalking()
-	{
-		if (AnimationStates == AnimationState.Punching_1) return;
-		if (AnimationStates == AnimationState.Punching_2) return;
-		PlayAnimation(AnimationState.Idle);
-	}
-
-
-	private void faceTarget(Vector3 etarget, float espeed)
-	{
-		Vector3 targetDir = etarget - this.transform.position;
-		float step = espeed * Time.deltaTime;
-		Vector3 newDir = Vector3.RotateTowards(this.transform.forward, targetDir, step, 0.0f);
-		this.transform.rotation = Quaternion.LookRotation(newDir);
-	}
+    internal void StopWalking()
+    {
+        if (AnimationStates == AnimationState.Punching_1) return;
+        if (AnimationStates == AnimationState.Punching_2) return;
+        PlayAnimation(AnimationState.Idle);
+    }
 
 
-	void OnTriggerEnter(Collider other)
-	{
-		if (other.transform.tag != "Player") return;
+    private void faceTarget(Vector3 etarget, float espeed)
+    {
+        Vector3 targetDir = etarget - this.transform.position;
+        float step = espeed * Time.deltaTime;
+        Vector3 newDir = Vector3.RotateTowards(this.transform.forward, targetDir, step, 0.0f);
+        this.transform.rotation = Quaternion.LookRotation(newDir);
+    }
 
-		biology biology = other.transform.root.GetComponent<biology>();
-		if (biology == this) return;
 
-		AnimationState AnimationStates = biology.AnimationStates;
-		if (AnimationStates != AnimationState.Punching_1 && AnimationStates != AnimationState.Punching_2) return;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag != "Player") return;
 
-		if (biology.IsMoveable == true) return;
+        biology biology = other.transform.root.GetComponent<biology>();
+        if (biology == this) return;
 
-		PlayAnimation(AnimationState.HurtUp);
-		transform.LookAt(other.transform.root);
-		transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+        AnimationState AnimationStates = biology.AnimationStates;
+        if (AnimationStates != AnimationState.Punching_1 && AnimationStates != AnimationState.Punching_2) return;
 
-		StopAnimator();
-		biology.StopAnimator();
-		biology.IsMoveable = true;
-	}
+        if (biology.IsMoveable == true) return;
 
-	internal void StopAnimator()
-	{
-		HitStopTime = main.HitStopTime;
-		coroutine = wait();
-		StartCoroutine(coroutine);
-	}
+        PlayAnimation(AnimationState.HurtUp);
+        transform.LookAt(other.transform.root);
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
 
-	private float waitCounter = 0;
-	IEnumerator wait()
-	{
-		waitCounter = 0;
-		Animator.enabled = false;
-		while (waitCounter < HitStopTime)
-		{
-			Debug.Log(waitCounter);
-			waitCounter += Time.unscaledDeltaTime;
+        StopAnimator();
+        StartShake(other);
+        biology.StopAnimator();
+        GameObject Effect = Instantiate(main.Effects[2]);
+        Effect.transform.position = other.ClosestPoint(transform.position);
+        Effect.AddComponent<ParticleSystemAutoDestroy>();
+        biology.IsMoveable = true;
+    }
 
-			//Yield until the next frame
-			yield return null;
-		}
-		Animator.enabled = true;
-	}
+    internal void StopAnimator()
+    {
+        HitStopTime = main.HitStopTime;
+        StartCoroutine("Wait");
+    }
+    internal void StartShake(Collider other)
+    {
+        StartCoroutine("Shake", other);
+    }
 
+    IEnumerator Wait()
+    {
+        float waitCounter = 0;
+        Animator.enabled = false;
+        while (waitCounter < HitStopTime)
+        {
+            waitCounter += Time.fixedDeltaTime;
+            //Yield until the next frame
+            yield return null;
+        }
+        Animator.enabled = true;
+    }
+
+    IEnumerator Shake(Collider other)
+    {
+        float waitCounter = 0;
+        Vector3 _position = transform.position;
+        while (waitCounter < HitStopTime)
+        {
+            float r = UnityEngine.Random.Range(-0.05f, -0.01f);
+            transform.position = _position + other.transform.forward * r;
+            waitCounter += Time.fixedDeltaTime;
+            //Yield until the next frame
+            yield return null;
+        }
+        transform.position = _position;
+    }
 
 }
--- Assets/script/main.cs
+++ Assets/script/main.cs
@@ -4,156 +4,159 @@ using UnityEngine;
 
 public class main : MonoBehaviour
 {
-	private float KeyboardMoveSpeed = 3.5f;
-	private visualJoyStick visualJoyStick;
-	private cameraScript cam;
-	private cameraZLookScript camZLook;
-	[SerializeField] private biology player;
-	private GameObject[] targets;
-	private GameObject target;
-	bool isZLook;
-	[SerializeField] private UnityEngine.UI.Slider Slider;
-	internal float HitStopTime;
+    private float KeyboardMoveSpeed = 3.5f;
+    private visualJoyStick visualJoyStick;
+    private cameraScript cam;
+    private cameraZLookScript camZLook;
+    [SerializeField] private biology player;
+    private GameObject[] targets;
+    private GameObject target;
+    bool isZLook;
+    [SerializeField] private UnityEngine.UI.Slider Slider;
+    internal float HitStopTime;
 
-	// Use this for initialization
-	private void Start()
-	{
-		HitStopTime = Slider.value;
-		targets = GameObject.FindGameObjectsWithTag("blue");
-		cam = GameObject.Find("Main Camera").GetComponent<cameraScript>();
-		camZLook = GameObject.Find("cameraZLook").GetComponent<cameraZLookScript>();
-		visualJoyStick = GameObject.Find("visualJoyStick").GetComponent<visualJoyStick>();
-		cam.setTarget(player.transform);
-		camZLook.setFollowTarget(player.transform);
-	}
+    [SerializeField]
+    internal List<GameObject> Effects = new List<GameObject>();
 
-	// Update is called once per frame
-	private void Update()
-	{
-		controlPlayer();
-		searchClosetTarget();
-		// showTarget();
-	}
+    // Use this for initialization
+    private void Start()
+    {
+        HitStopTime = Slider.value;
+        targets = GameObject.FindGameObjectsWithTag("blue");
+        cam = GameObject.Find("Main Camera").GetComponent<cameraScript>();
+        camZLook = GameObject.Find("cameraZLook").GetComponent<cameraZLookScript>();
+        visualJoyStick = GameObject.Find("visualJoyStick").GetComponent<visualJoyStick>();
+        cam.setTarget(player.transform);
+        camZLook.setFollowTarget(player.transform);
+    }
 
-	private void searchClosetTarget()
-	{
-		if (isZLook) return;
+    // Update is called once per frame
+    private void Update()
+    {
+        controlPlayer();
+        searchClosetTarget();
+        // showTarget();
+    }
 
-		float dist = Mathf.Infinity;
-		GameObject tempTarget = null;
-		foreach (var t in targets)
-		{
-			float temp = Vector3.Distance(t.transform.position, player.transform.position);
-			if (temp < dist)
-			{
-				tempTarget = t;
-				dist = temp;
-			}
-		}
-		target = tempTarget;
+    private void searchClosetTarget()
+    {
+        if (isZLook) return;
 
-	}
+        float dist = Mathf.Infinity;
+        GameObject tempTarget = null;
+        foreach (var t in targets)
+        {
+            float temp = Vector3.Distance(t.transform.position, player.transform.position);
+            if (temp < dist)
+            {
+                tempTarget = t;
+                dist = temp;
+            }
+        }
+        target = tempTarget;
 
-	private void showTarget()
-	{
-		GameObject.Find("Cube").transform.position = target.transform.position + Vector3.up * 1.2f;
-	}
+    }
 
-	private void controlPlayer()
-	{
-		Vector3 newDirect = Vector3.zero;
+    private void showTarget()
+    {
+        GameObject.Find("Cube").transform.position = target.transform.position + Vector3.up * 1.2f;
+    }
 
-		if (Input.GetKey("left shift"))
-		{
-			useZLookCam();
-			camZLook.setZLookOn(target);
-			isZLook = true;
-		}
-		if (Input.GetKeyUp("left shift"))
-		{
-			useNormalCam();
-			isZLook = false;
-		}
-		if (visualJoyStick.touch)
-		{
-			//如果沒在注視模式時，使用一般攝影機座標
-			newDirect = transformJoyStickSpace(visualJoyStick.joyStickVec, cam.transform);
-			//如果正在注視模式時，使用注視攝影機座標
-			if (isZLook) newDirect = transformJoyStickSpace(visualJoyStick.joyStickVec, camZLook.transform);
-			player.MoveTo(newDirect / 2.0f);//fixme:0.25要放到外部控制
-		}
-		if (Input.GetKey("w"))
-		{
-			newDirect = transformJoyStickSpace(Vector2.up, cam.transform);
-			player.MoveTo(newDirect * 0.5f * KeyboardMoveSpeed);//fixme:0.25要放到外部控制
-		}
-		if (Input.GetKey("a"))
-		{
-			newDirect = transformJoyStickSpace(Vector2.left, cam.transform);
-			player.MoveTo(newDirect * 0.5f * KeyboardMoveSpeed);//fixme:0.25要放到外部控制
-		}
-		if (Input.GetKey("s"))
-		{
-			newDirect = transformJoyStickSpace(Vector2.down, cam.transform);
-			player.MoveTo(newDirect * 0.5f * KeyboardMoveSpeed);//fixme:0.25要放到外部控制
-		}
-		if (Input.GetKey("d"))
-		{
-			newDirect = transformJoyStickSpace(Vector2.right, cam.transform);
-			player.MoveTo(newDirect * 0.5f * KeyboardMoveSpeed);//fixme:0.25要放到外部控制s
-		}
-		if (Input.GetAxis("Mouse ScrollWheel") > 0)
-		{
-			KeyboardMoveSpeed += 0.1f;
-			KeyboardMoveSpeed = Mathf.Clamp(KeyboardMoveSpeed, 1, 6);
-		}
-		if (Input.GetAxis("Mouse ScrollWheel") < 0)
-		{
-			KeyboardMoveSpeed -= 0.1f;
-			KeyboardMoveSpeed = Mathf.Clamp(KeyboardMoveSpeed, 1, 6);
-		}
-		if (Input.GetMouseButtonDown(1))
-		{
-			player.AttackButtonDown();
-		}
-		if (Input.anyKey == false)
-		{
-			player.StopWalking();
-		}
-	}
-	private void useZLookCam()
-	{
-		cam.GetComponent<Camera>().enabled = false;
-		camZLook.GetComponent<Camera>().enabled = true;
-	}
-	private void useNormalCam()
-	{
-		cam.setPos(camZLook.transform);
-		cam.GetComponent<Camera>().enabled = true;
-		camZLook.GetComponent<Camera>().enabled = false;
-	}
+    private void controlPlayer()
+    {
+        Vector3 newDirect = Vector3.zero;
 
-	//將 參數一 的2D向量，改為 參數二 Transform為座標空間
-	Vector3 transformJoyStickSpace(Vector2 vec, Transform t)
-	{
-		//取得Transform t的朝前、朝右向量
-		Vector3 forward = new Vector3(t.transform.forward.x, 0, t.transform.forward.z);
-		Vector3 right = new Vector3(t.transform.right.x, 0, t.transform.right.z);
+        if (Input.GetKey("left shift"))
+        {
+            useZLookCam();
+            camZLook.setZLookOn(target);
+            isZLook = true;
+        }
+        if (Input.GetKeyUp("left shift"))
+        {
+            useNormalCam();
+            isZLook = false;
+        }
+        if (visualJoyStick.touch)
+        {
+            //如果沒在注視模式時，使用一般攝影機座標
+            newDirect = transformJoyStickSpace(visualJoyStick.joyStickVec, cam.transform);
+            //如果正在注視模式時，使用注視攝影機座標
+            if (isZLook) newDirect = transformJoyStickSpace(visualJoyStick.joyStickVec, camZLook.transform);
+            player.MoveTo(newDirect / 2.0f);//fixme:0.25要放到外部控制
+        }
+        if (Input.GetKey("w"))
+        {
+            newDirect = transformJoyStickSpace(Vector2.up, cam.transform);
+            player.MoveTo(newDirect * 0.5f * KeyboardMoveSpeed);//fixme:0.25要放到外部控制
+        }
+        if (Input.GetKey("a"))
+        {
+            newDirect = transformJoyStickSpace(Vector2.left, cam.transform);
+            player.MoveTo(newDirect * 0.5f * KeyboardMoveSpeed);//fixme:0.25要放到外部控制
+        }
+        if (Input.GetKey("s"))
+        {
+            newDirect = transformJoyStickSpace(Vector2.down, cam.transform);
+            player.MoveTo(newDirect * 0.5f * KeyboardMoveSpeed);//fixme:0.25要放到外部控制
+        }
+        if (Input.GetKey("d"))
+        {
+            newDirect = transformJoyStickSpace(Vector2.right, cam.transform);
+            player.MoveTo(newDirect * 0.5f * KeyboardMoveSpeed);//fixme:0.25要放到外部控制s
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            KeyboardMoveSpeed += 0.1f;
+            KeyboardMoveSpeed = Mathf.Clamp(KeyboardMoveSpeed, 1, 6);
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            KeyboardMoveSpeed -= 0.1f;
+            KeyboardMoveSpeed = Mathf.Clamp(KeyboardMoveSpeed, 1, 6);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            player.AttackButtonDown();
+        }
+        if (Input.anyKey == false)
+        {
+            player.StopWalking();
+        }
+    }
+    private void useZLookCam()
+    {
+        cam.GetComponent<Camera>().enabled = false;
+        camZLook.GetComponent<Camera>().enabled = true;
+    }
+    private void useNormalCam()
+    {
+        cam.setPos(camZLook.transform);
+        cam.GetComponent<Camera>().enabled = true;
+        camZLook.GetComponent<Camera>().enabled = false;
+    }
 
-		//將左右向量標準化（變成最短）
-		forward = Vector3.Normalize(forward);
-		right = Vector3.Normalize(right);
+    //將 參數一 的2D向量，改為 參數二 Transform為座標空間
+    Vector3 transformJoyStickSpace(Vector2 vec, Transform t)
+    {
+        //取得Transform t的朝前、朝右向量
+        Vector3 forward = new Vector3(t.transform.forward.x, 0, t.transform.forward.z);
+        Vector3 right = new Vector3(t.transform.right.x, 0, t.transform.right.z);
 
-		//將標準化左右向量加上搖桿的放大量
-		right *= vec.x;
-		forward *= vec.y;
+        //將左右向量標準化（變成最短）
+        forward = Vector3.Normalize(forward);
+        right = Vector3.Normalize(right);
 
-		return forward + right;
-	}
+        //將標準化左右向量加上搖桿的放大量
+        right *= vec.x;
+        forward *= vec.y;
 
-	public void SetHitStopTime()
-	{
-		HitStopTime = Slider.value;
-	}
+        return forward + right;
+    }
+
+    public void SetHitStopTime()
+    {
+        HitStopTime = Slider.value;
+    }
 
 }
