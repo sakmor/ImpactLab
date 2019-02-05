723ce73a60f3d4fb4079b927bd62b15c94d01c56
王山姆 <sakmor@gmail.com>
2019-02-06 00:38:28 +0800

    優化中'

--- Assets/script/cameraZLookScript.cs
+++ Assets/script/cameraZLookScript.cs
@@ -4,62 +4,45 @@ using UnityEngine;
 
 public class cameraZLookScript : MonoBehaviour
 {
-    public GameObject lookTarget, followTarget;
+    private biology BiologyPlayer;
     Vector3 cam2Target;
-    bool isZLook;
+
 
     // Use this for initialization
     void Start()
     {
-        isZLook = false;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isZLook)
-        {
-            set2FollwTargetBack();
-            transform.LookAt(lookTarget.transform);
-        }
+        set2FollwTargetBack();
+        // transform.LookAt(lookTarget.transform.Find("N1").gameObject.transform);
     }
-    void cameraFollow()
-    {
-        transform.position = followTarget.transform.position + cam2Target;
-    }
-    void setCam2target()
-    {
-        cam2Target = transform.position - followTarget.transform.position;
-    }
-    void setLookTarget(GameObject n)
-    {
-        lookTarget = n.transform.Find("N1").gameObject;  //todo:當目標沒有N1點的時候會當機
-    }
+    // void cameraFollow()
+    // {
+    //     transform.position = followTarget.transform.position + cam2Target;
+    // }
+    // void setCam2target()
+    // {
+    //     cam2Target = transform.position - followTarget.transform.position;
+    // }
+
     void set2FollwTargetBack()
     {
-        Vector3 back = lookTarget.transform.position - followTarget.transform.position;
+        Vector3 back = BiologyPlayer.Target.transform.position - BiologyPlayer.transform.position;
         back = new Vector3(back.x, 0, back.z);
         back = Vector3.Normalize(back);
         back = back * -1.25f;
-        transform.position = followTarget.transform.position + back + Vector3.up * 1.25f;
+        transform.position = BiologyPlayer.transform.position + back + Vector3.up * 1.25f;
+        transform.LookAt(BiologyPlayer.Target.transform.Find("N1").position);
+        BiologyPlayer.transform.LookAt(BiologyPlayer.Target.transform.position);
     }
 
-    public void setFollowTarget(Transform n)
+    public void SetPlayer(biology player)
     {
-        followTarget = n.gameObject;
+        BiologyPlayer = player;
     }
 
-    public void setZLookOff()
-    {
-        isZLook = false;
-    }
-    public void setZLookOn(GameObject t)
-    {
-        if (!isZLook)
-        {
-            isZLook = true;
-        }
-        setLookTarget(t);
-    }
 }
--- Assets/script/main.cs
+++ Assets/script/main.cs
@@ -1,174 +1,160 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class main : MonoBehaviour
 {
-	private float KeyboardMoveSpeed = 3.5f;
-	[SerializeField] private biology player;
-	[SerializeField] internal float ShakeTimes, ShakePower;
-	private GameObject[] targets;
-	private GameObject target;
-	bool isZLook;
-	[SerializeField] private UnityEngine.UI.Slider Slider;
-	[SerializeField] private UnityEngine.UI.Toggle CameraShake_Toggle;
-	[SerializeField] private UnityEngine.UI.Toggle HittedShake_Toggle;
-	[SerializeField] private UnityEngine.UI.Toggle HittedFlash_Toggle;
-	[SerializeField] private UnityEngine.UI.Toggle HittedFx_Toggle;
-	[SerializeField] internal float HitStopTime;
-	public bool IsCameraShake, IsHittedShake, IsHittedFlash, IsHittedFx;
+    private float KeyboardMoveSpeed = 3.5f;
+    [SerializeField] private biology Player;
+    [SerializeField] internal float ShakeTimes, ShakePower;
+    [SerializeField] private GameObject TaregtMark;
+    internal GameObject[] EmenyBiologys;
+    private GameObject target;
+    bool isZLook;
+    [SerializeField] private UnityEngine.UI.Slider Slider;
+    [SerializeField] private UnityEngine.UI.Toggle CameraShake_Toggle;
+    [SerializeField] private UnityEngine.UI.Toggle HittedShake_Toggle;
+    [SerializeField] private UnityEngine.UI.Toggle HittedFlash_Toggle;
+    [SerializeField] private UnityEngine.UI.Toggle HittedFx_Toggle;
+    [SerializeField] internal float HitStopTime;
+    public bool IsCameraShake, IsHittedShake, IsHittedFlash, IsHittedFx;
 
-	[SerializeField] public cameraScript cam;
-	[SerializeField] private cameraZLookScript camZLook;
-	[SerializeField] private visualJoyStick visualJoyStick;
-	[SerializeField]
-	internal List<GameObject> Effects = new List<GameObject>();
+    [SerializeField] public cameraScript cam;
+    [SerializeField] private cameraZLookScript camZLook;
+    [SerializeField] private visualJoyStick visualJoyStick;
+    [SerializeField]
+    internal List<GameObject> Effects = new List<GameObject>();
 
-	// Use this for initialization
-	private void Start()
-	{
-		UpdateSettingValue();
-		targets = GameObject.FindGameObjectsWithTag("blue");
-		cam.setTarget(player.CameraPoint);
-		camZLook.setFollowTarget(player.transform);
-	}
+    // Use this for initialization
+    private void Start()
+    {
+        UpdateSettingValue();
+        EmenyBiologys = GameObject.FindGameObjectsWithTag("blue");
+        SetupCameraSystem();
 
-	// Update is called once per frame
-	private void Update()
-	{
-		controlPlayer();
-		searchClosetTarget();
-		// showTarget();
-	}
+    }
 
-	private void searchClosetTarget()
-	{
-		if (isZLook) return;
+    // Update is called once per frame
+    private void Update()
+    {
+        controlPlayer();
+        Player.searchClosetTarget();
+        showTarget();
+    }
 
-		float dist = Mathf.Infinity;
-		GameObject tempTarget = null;
-		foreach (var t in targets)
-		{
-			float temp = Vector3.Distance(t.transform.position, player.transform.position);
-			if (temp < dist)
-			{
-				tempTarget = t;
-				dist = temp;
-			}
-		}
-		target = tempTarget;
+    private void SetupCameraSystem()
+    {
+        cam.setTarget(Player.CameraPoint);
+        camZLook.SetPlayer(Player);
+    }
 
-	}
+    private void showTarget()
+    {
+        TaregtMark.transform.position = Player.Target.transform.position + Vector3.up * 1.2f;
+    }
 
-	private void showTarget()
-	{
-		GameObject.Find("Cube").transform.position = target.transform.position + Vector3.up * 1.2f;
-	}
+    private void controlPlayer()
+    {
+        Vector3 newDirect = Vector3.zero;
 
-	private void controlPlayer()
-	{
-		Vector3 newDirect = Vector3.zero;
+        if (Input.GetKey("left shift"))
+        {
+            useZLookCam();
+            isZLook = true;
+        }
+        if (Input.GetKeyUp("left shift"))
+        {
+            useNormalCam();
+            isZLook = false;
+        }
+        if (visualJoyStick.touch)
+        {
+            //如果沒在注視模式時，使用一般攝影機座標
+            newDirect = transformJoyStickSpace(visualJoyStick.joyStickVec, cam.transform);
+            //如果正在注視模式時，使用注視攝影機座標
+            if (isZLook) newDirect = transformJoyStickSpace(visualJoyStick.joyStickVec, camZLook.transform);
+            Player.MoveTo(newDirect / 2.0f);//fixme:0.25要放到外部控制
+        }
+        if (Input.GetKey("w"))
+        {
+            newDirect = transformJoyStickSpace(Vector2.up, cam.transform);
+            Player.MoveTo(newDirect * 0.5f * KeyboardMoveSpeed);//fixme:0.25要放到外部控制
+        }
+        if (Input.GetKey("a"))
+        {
+            newDirect = transformJoyStickSpace(Vector2.left, cam.transform);
+            Player.MoveTo(newDirect * 0.5f * KeyboardMoveSpeed);//fixme:0.25要放到外部控制
+        }
+        if (Input.GetKey("s"))
+        {
+            newDirect = transformJoyStickSpace(Vector2.down, cam.transform);
+            Player.MoveTo(newDirect * 0.5f * KeyboardMoveSpeed);//fixme:0.25要放到外部控制
+        }
+        if (Input.GetKey("d"))
+        {
+            newDirect = transformJoyStickSpace(Vector2.right, cam.transform);
+            Player.MoveTo(newDirect * 0.5f * KeyboardMoveSpeed);//fixme:0.25要放到外部控制s
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            KeyboardMoveSpeed += 0.1f;
+            KeyboardMoveSpeed = Mathf.Clamp(KeyboardMoveSpeed, 1, 6);
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            KeyboardMoveSpeed -= 0.1f;
+            KeyboardMoveSpeed = Mathf.Clamp(KeyboardMoveSpeed, 1, 6);
+        }
+        if (Input.GetMouseButton(1))
+        {
+            Player.AttackButtonDown();
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            Player.SpaceButtonDown();
+        }
+        if (Input.anyKey == false)
+        {
+            Player.StopWalking();
+        }
+    }
+    private void useZLookCam()
+    {
+        cam.gameObject.SetActive(false);
+        camZLook.gameObject.SetActive(true);
+    }
+    private void useNormalCam()
+    {
+        cam.setPos(camZLook.transform);
+        cam.gameObject.SetActive(true);
+        camZLook.gameObject.SetActive(false);
+    }
 
-		if (Input.GetKey("left shift"))
-		{
-			useZLookCam();
-			camZLook.setZLookOn(target);
-			isZLook = true;
-		}
-		if (Input.GetKeyUp("left shift"))
-		{
-			useNormalCam();
-			isZLook = false;
-		}
-		if (visualJoyStick.touch)
-		{
-			//如果沒在注視模式時，使用一般攝影機座標
-			newDirect = transformJoyStickSpace(visualJoyStick.joyStickVec, cam.transform);
-			//如果正在注視模式時，使用注視攝影機座標
-			if (isZLook) newDirect = transformJoyStickSpace(visualJoyStick.joyStickVec, camZLook.transform);
-			player.MoveTo(newDirect / 2.0f);//fixme:0.25要放到外部控制
-		}
-		if (Input.GetKey("w"))
-		{
-			newDirect = transformJoyStickSpace(Vector2.up, cam.transform);
-			player.MoveTo(newDirect * 0.5f * KeyboardMoveSpeed);//fixme:0.25要放到外部控制
-		}
-		if (Input.GetKey("a"))
-		{
-			newDirect = transformJoyStickSpace(Vector2.left, cam.transform);
-			player.MoveTo(newDirect * 0.5f * KeyboardMoveSpeed);//fixme:0.25要放到外部控制
-		}
-		if (Input.GetKey("s"))
-		{
-			newDirect = transformJoyStickSpace(Vector2.down, cam.transform);
-			player.MoveTo(newDirect * 0.5f * KeyboardMoveSpeed);//fixme:0.25要放到外部控制
-		}
-		if (Input.GetKey("d"))
-		{
-			newDirect = transformJoyStickSpace(Vector2.right, cam.transform);
-			player.MoveTo(newDirect * 0.5f * KeyboardMoveSpeed);//fixme:0.25要放到外部控制s
-		}
-		if (Input.GetAxis("Mouse ScrollWheel") > 0)
-		{
-			KeyboardMoveSpeed += 0.1f;
-			KeyboardMoveSpeed = Mathf.Clamp(KeyboardMoveSpeed, 1, 6);
-		}
-		if (Input.GetAxis("Mouse ScrollWheel") < 0)
-		{
-			KeyboardMoveSpeed -= 0.1f;
-			KeyboardMoveSpeed = Mathf.Clamp(KeyboardMoveSpeed, 1, 6);
-		}
-		if (Input.GetMouseButton(1))
-		{
-			player.AttackButtonDown();
-		}
-		if (Input.GetKeyDown("space"))
-		{
-			player.SpaceButtonDown();
-		}
-		if (Input.anyKey == false)
-		{
-			player.StopWalking();
-		}
-	}
-	private void useZLookCam()
-	{
-		cam.GetComponent<Camera>().enabled = false;
-		camZLook.GetComponent<Camera>().enabled = true;
-	}
-	private void useNormalCam()
-	{
-		cam.setPos(camZLook.transform);
-		cam.GetComponent<Camera>().enabled = true;
-		camZLook.GetComponent<Camera>().enabled = false;
-	}
+    //將 參數一 的2D向量，改為 參數二 Transform為座標空間
+    Vector3 transformJoyStickSpace(Vector2 vec, Transform t)
+    {
+        //取得Transform t的朝前、朝右向量
+        Vector3 forward = new Vector3(t.transform.forward.x, 0, t.transform.forward.z);
+        Vector3 right = new Vector3(t.transform.right.x, 0, t.transform.right.z);
 
-	//將 參數一 的2D向量，改為 參數二 Transform為座標空間
-	Vector3 transformJoyStickSpace(Vector2 vec, Transform t)
-	{
-		//取得Transform t的朝前、朝右向量
-		Vector3 forward = new Vector3(t.transform.forward.x, 0, t.transform.forward.z);
-		Vector3 right = new Vector3(t.transform.right.x, 0, t.transform.right.z);
+        //將左右向量標準化（變成最短）
+        forward = Vector3.Normalize(forward);
+        right = Vector3.Normalize(right);
 
-		//將左右向量標準化（變成最短）
-		forward = Vector3.Normalize(forward);
-		right = Vector3.Normalize(right);
+        //將標準化左右向量加上搖桿的放大量
+        right *= vec.x;
+        forward *= vec.y;
 
-		//將標準化左右向量加上搖桿的放大量
-		right *= vec.x;
-		forward *= vec.y;
+        return forward + right;
+    }
 
-		return forward + right;
-	}
-
-	public void UpdateSettingValue()
-	{
-		HitStopTime = Slider.value;
-		IsCameraShake = CameraShake_Toggle.isOn;
-		IsHittedShake = HittedShake_Toggle.isOn;
-		IsHittedFlash = HittedFlash_Toggle.isOn;
-		IsHittedFx = HittedFx_Toggle.isOn;
-	}
+    public void UpdateSettingValue()
+    {
+        HitStopTime = Slider.value;
+        IsCameraShake = CameraShake_Toggle.isOn;
+        IsHittedShake = HittedShake_Toggle.isOn;
+        IsHittedFlash = HittedFlash_Toggle.isOn;
+        IsHittedFx = HittedFx_Toggle.isOn;
+    }
 
 }
